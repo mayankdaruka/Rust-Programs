d67f31573a403d16db95d240b914756cf7f94b8d
Mayank Daruka <mayankdaruka@utexas.edu>
2022-01-02 15:36:20 -0600

    finished up chapter 4 notes

--- chapter_four.rs
+++ chapter_four.rs
@@ -14,6 +14,17 @@
  * variable remains valid until it goes out of scope
  */
 
+/** can only have 1 mutable reference to a piece of data at a time
+ * prevents a data race, which happens when:
+ * two or more pointers access the data at the same time
+ * at least 1 of the pointers is being used to write to the data
+ * no mechanism being used to synchronize access to the data
+ */
+
+/** however -
+ * multiple immutable references are okay
+ */
+
 fn main() {
     // first string type: string literals - immutable
     // second string type: String - manages data allocated on heap (can be mutated as well)
@@ -51,7 +62,7 @@ fn main() {
     println!("Value of x: {}", x);
 
     let s1 = String::from("test");
-    let s2 = takes_and_gives_ownership(s1);
+    let mut s2 = takes_and_gives_ownership(s1);
 
     // println!("s1: {}", s1); // value moved to s2, so s1 not valid anymore
     println!("s2: {}", s2);
@@ -59,6 +70,42 @@ fn main() {
     // what if we wanted to pass a value to a function but not let the function take ownership of it?
     let len = calculate_length(&s2);
     println!("The length of {} is: {}", s2, len); 
+
+    // change(&s2); // references and variables are both immutable by default
+    change(&mut s2);
+
+    println!("s2: {}", s2);
+
+    let mut s = String::from("hello");
+
+    // can only have 1 mutable reference to a particular piece of data at a time
+    // let r1 = &mut s;
+    let r2 = &mut s;
+    // let r3 = &s;
+
+    println!("Value of r3: {}", r2);
+    // variable r2 will not be used after this point
+
+    // Cannot have a mutable reference while we have an immutable one
+    // println!("Values: {} and {}", r2, r3); // cannot borrow s as immutable since it is already borrowed as mutable
+
+    let r4 = &s;
+    let r5 = &s;
+
+    println!("{} and {}", r4, r5);
+
+    // rust prevents dangling pointers
+    // let reference_to_nothing = dangle();
+
+    // println!("Reference to nothing: {}", reference_to_nothing); // missing lifetime specifier
+
+
+    let s = String::from("Rust is fun");
+
+    // a string slice is a reference to part of a String
+    // string literals are string slices already
+    let second_word = &s[5..7];
+    println!("{}", second_word);
 
 } // memory is automatically returned once variable that owns it goes out of scope (drop function in rust)
 
@@ -78,3 +125,12 @@ fn takes_and_gives_ownership(some_string: String) -> String {
 fn calculate_length(s: &String) -> usize {
     s.len()
 }
+
+fn change(some_string: &mut String) {
+    some_string.push_str(" testing"); 
+}
+
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+//     &s
+// }
